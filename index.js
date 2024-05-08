331864ab8be36984edad85a8da9df55e42211105
hoangduc19867 <caysonhandpun0suy13@hotmail.com>
2024-05-08 10:33:45 +0700

    update task

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const heapSort = (arr) => {
-  const heapify = (arr, n, i) => {
-    let largest = i;
-    const left = 2 * i + 1;
-    const right = 2 * i + 2;
-    if (left < n && arr[left] > arr[largest]) {
-      largest = left;
+function generate(numRows) {
+  const triangle = [];
+  for (let i = 0; i < numRows; i++) {
+    const row = new Array(i + 1).fill(1);
+    for (let j = 1; j < row.length - 1; j++) {
+      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
     }
-    if (right < n && arr[right] > arr[largest]) {
-      largest = right;
-    }
-    if (largest !== i) {
-      [arr[i], arr[largest]] = [arr[largest], arr[i]];
-      heapify(arr, n, largest);
-    }
-  };
-  const buildHeap = (arr) => {
-    const n = arr.length;
-    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
-      heapify(arr, n, i);
-    }
-  };
-  buildHeap(arr);
-  for (let i = arr.length - 1; i > 0; i--) {
-    [arr[0], arr[i]] = [arr[i], arr[0]];
-    heapify(arr, i, 0);
+    triangle.push(row);
   }
-  return arr;
-};
+  return triangle;
+}

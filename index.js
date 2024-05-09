45a0911420fe5142303f3a16ea80805989e04b91
hoangduc19867 <caysonhandpun0suy13@hotmail.com>
2024-05-09 18:42:58 +0700

    update task

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-function generate(numRows) {
-  const triangle = [];
-  for (let i = 0; i < numRows; i++) {
-    const row = new Array(i + 1).fill(1);
-    for (let j = 1; j < row.length - 1; j++) {
-      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-    }
-    triangle.push(row);
-  }
-  return triangle;
+function findKthLargest(nums, k) {
+  nums.sort((a, b) => b - a);
+  return nums[k - 1];
 }

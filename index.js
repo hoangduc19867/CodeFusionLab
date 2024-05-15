5ad3cb18ccbd46c3c5e7e673c103e47000d970ed
hoangduc19867 <caysonhandpun0suy13@hotmail.com>
2024-05-15 20:14:35 +0700

    feat task

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function reverseBetween(head, m, n) {
-  if (!head || m === n) return head;
-  const dummy = new ListNode(0);
-  dummy.next = head;
-  let prev = dummy;
-  for (let i = 0; i < m - 1; i++) prev = prev.next;
-  let start = prev.next;
-  let then = start.next;
-  for (let i = 0; i < n - m; i++) {
-    start.next = then.next;
-    then.next = prev.next;
-    prev.next = then;
-    then = start.next;
-  }
-  return dummy.next;
-}
+// Setup the client
+const aptosConfig = new AptosConfig({ network: APTOS_NETWORK });
+const aptos = new Aptos(aptosConfig);
